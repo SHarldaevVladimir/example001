d4efe64bc8fb2872f2e84c1d93196dca61cf2ea8
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-10-13 11:39:40 +0300

    add example 005

---
+++ example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователья");
+string username = Console.ReadLine();
+
+ if(username.ToLower() == "Маша")
+
+    {
+        Console.WriteLine("Ура, это же Маша!");
+    }
+
+else 
+    {
+        Console.WriteLine("Привет, ");
+        Console.WriteLine(username);
+    }
